ece6be26c555e8621cdaa6fec59e7297d5f6796e
Bob2345de <Bob2345de@users.noreply.github.com>
2025-02-11 12:51:15 +0100

    32 bit done with usb_to_cpp

--- lib/fanatec_utilities/fanatec_utilities.cpp
+++ lib/fanatec_utilities/fanatec_utilities.cpp
@@ -7,12 +7,11 @@ void initialize_default_configs() {
     if (!SPIFFS.exists(CONFIG_FILE)) {
         FanatecConfig configs[MAX_CONFIGS];
         
-        // Initialize all configs
         for (int i = 0; i < MAX_CONFIGS; i++) {
             snprintf(configs[i].configname, 10, "Config_%d", i+1);
             configs[i].config_number = i+1;
             configs[i].wheel_ident = 0x03;
-            memset(configs[i].transpose_array, 0x00, 3);
+            memset(configs[i].transpose_array, 0x00, 4);
         }
         
         File file = SPIFFS.open(CONFIG_FILE, "w");
@@ -25,12 +24,9 @@ void initialize_default_configs() {
     }
 }
 
-// Store active config number at end of configs array (offset 180 = 10 configs * 18 bytes each)
 bool store_active_config(int active_config) {
     File file = SPIFFS.open(CONFIG_FILE, "r+");
-    if (!file) {
-        file = SPIFFS.open(CONFIG_FILE, "w+");
-    }
+    if (!file) file = SPIFFS.open(CONFIG_FILE, "w+");
     
     if (file) {
         file.seek(MAX_CONFIGS * sizeof(FanatecConfig));
@@ -54,23 +50,20 @@ int retrieve_active_config() {
     return active_config;
 }
 
-void store_config(const char name[10], int config_num, uint8_t wheel_id, const uint8_t transpose[3]) {
+void store_config(const char name[10], int config_num, uint8_t wheel_id, const uint8_t transpose[4]) {
     FanatecConfig new_config;
     memcpy(new_config.configname, name, 10);
     new_config.config_number = config_num;
     new_config.wheel_ident = wheel_id;
-    memcpy(new_config.transpose_array, transpose, 3);
+    memcpy(new_config.transpose_array, transpose, 4);
 
     File file = SPIFFS.open(CONFIG_FILE, "r+");
-    if (!file) {
-        file = SPIFFS.open(CONFIG_FILE, "w+");
-    }
+    if (!file) file = SPIFFS.open(CONFIG_FILE, "w+");
     
     if (file) {
         FanatecConfig configs[MAX_CONFIGS];
         file.read((uint8_t*)configs, sizeof(configs));
         
-        // Update existing or find empty slot
         bool updated = false;
         for (int i = 0; i < MAX_CONFIGS; i++) {
             if (configs[i].config_number == config_num || configs[i].config_number == 0) {
@@ -88,7 +81,7 @@ void store_config(const char name[10], int config_num, uint8_t wheel_id, const u
     }
 }
 
-bool retrieve_config(int config_num, char name_out[10], uint8_t* wheel_id, uint8_t transpose_out[3]) {
+bool retrieve_config(int config_num, char name_out[10], uint8_t* wheel_id, uint8_t transpose_out[4]) {
     File file = SPIFFS.open(CONFIG_FILE, "r");
     if (file) {
         FanatecConfig configs[MAX_CONFIGS];
@@ -99,7 +92,7 @@ bool retrieve_config(int config_num, char name_out[10], uint8_t* wheel_id, uint8
             if (configs[i].config_number == config_num) {
                 memcpy(name_out, configs[i].configname, 10);
                 *wheel_id = configs[i].wheel_ident;
-                memcpy(transpose_out, configs[i].transpose_array, 3);
+                memcpy(transpose_out, configs[i].transpose_array, 4);
                 return true;
             }
         }
@@ -107,9 +100,9 @@ bool retrieve_config(int config_num, char name_out[10], uint8_t* wheel_id, uint8
     return false;
 }
 
-void transposeBits(const uint8_t data[3], uint8_t dataout[3], const uint8_t transpose[24]) {
-  memset(dataout, 0, 3);
-  for (uint8_t i = 0; i < 24; i++) {
+void transposeBits(const uint8_t data[4], uint8_t dataout[4], const uint8_t transpose[32]) {
+  memset(dataout, 0, 4);
+  for (uint8_t i = 0; i < 32; i++) {
     uint8_t input_byte = i / 8;
     uint8_t input_bit = i % 8;
     if (data[input_byte] & (1 << input_bit)) {
--- lib/fanatec_utilities/fanatec_utilities.h
+++ lib/fanatec_utilities/fanatec_utilities.h
@@ -5,16 +5,23 @@
 #define MAX_CONFIGS 10
 #define CONFIG_FILE "/fanatec_configs.bin"
 
-typedef struct FanatecConfig {
+typedef struct {
     char configname[10];
     int config_number;
     uint8_t wheel_ident;
-    uint8_t transpose_array[3];
+    uint8_t transpose_array[4]; // 32-bit (4 byte) transpose mapping
 } FanatecConfig;
 
-void store_config(const char name[10], int config_num, uint8_t wheel_id, const uint8_t transpose[3]);
-bool retrieve_config(int config_num, char name_out[10], uint8_t* wheel_id, uint8_t transpose_out[3]);
+// Store/retrieve configuration with 4-byte transpose arrays
+void store_config(const char name[10], int config_num, uint8_t wheel_id, const uint8_t transpose[4]);
+bool retrieve_config(int config_num, char name_out[10], uint8_t* wheel_id, uint8_t transpose_out[4]);
+
+// Active config management
 bool store_active_config(int active_config);
 int retrieve_active_config();
+
+// Transpose function with 32-element mapping
+void transposeBits(const uint8_t data[4], uint8_t dataout[4], const uint8_t transpose[32]);
+
+// Initialization
 void initialize_default_configs();
-void transposeBits(const uint8_t data[3], uint8_t dataout[3], const uint8_t transpose[24]);
